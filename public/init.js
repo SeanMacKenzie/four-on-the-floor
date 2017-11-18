e5633c95f4e1e20797d93d7ff94f5e6eb84abfa2
Brandon Gulley <brandongulley@Brandons-MacBook-Pro.local>
2017-11-17 19:29:33 -0700

    updated reg

--- public/init.js
+++ public/init.js
@@ -5,32 +5,7 @@ var app ={
         authController: new AuthController()
     }
 }
-if(!false){
-    document.getElementById('nav-bar').innerHTML = `
-                            <form class="navbar-form navbar-left" onsubmit="app.controllers.authController.login(event)">
-                            <button class="btn btn-default" type="button">Register</button>         
-                            </form>
-                            <ul class="nav navbar-nav navbar-right">
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" id="dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown
-                                        <span class="caret"></span>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a href="#">Home</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Profile Page</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Favorites</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-    
-    `
-}
+
 function dropdown(){
     $('.dropdown').dropdown('toggle')
 }
